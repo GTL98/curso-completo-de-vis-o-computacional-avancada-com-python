36455fb7b1ce0d9c624eb77ec01829dc53a322e9
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2021-10-11 19:52:43 -0300

    Add files via upload

---
+++ Aula 4 - Estimativa de pose (Módulo).py
@@ -0,0 +1,156 @@
+################################################################################################
+#                                                                                              #
+#  Todo esse código será feito na extensão .py para que seja possível usá-lo nos projetos.     #
+#                                                                                              #
+#  Todas as anotações e explicações sobre o que está sendo usado nesse documento podem ser     #
+#  encontradas no documento "Aula 3 - Rastramento de pose (Introdução).ipynb".                 #
+#                                                                                              #
+#  GitHub: https://github.com/GTL98/curso-completo-de-visao-computacional-avancada-com-python  #
+#                                                                                              #
+################################################################################################
+
+
+import cv2
+import mediapipe as mp
+import time
+
+
+class DetectorPose:
+    def __init__(self, modo=False, complexidade=1, suavidade_landmarks=True, segmentacao=False,
+                 suavidade_segmentacao=True,
+                 deteccao_confianca=0.5, rastreamento_confianca=0.5, escala=40):
+        self.modo = modo
+        self.complexidade = complexidade
+        self.suavidade_landmarks = suavidade_landmarks
+        self.segmentacao = segmentacao
+        self.suavidade_segmentacao = suavidade_segmentacao
+        self.deteccao_confianca = deteccao_confianca
+        self.rastreamento_confianca = rastreamento_confianca
+        self.escala = escala
+        
+        # Pose
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.modo, self.complexidade, self.suavidade_landmarks,
+                                     self.segmentacao,
+                                     self.suavidade_segmentacao, self.deteccao_confianca,
+                                     self.rastreamento_confianca)
+
+        # Desenhar as landmarks
+        self.mp_desenho = mp.solutions.drawing_utils
+        
+    def encontrar_pose_camera(self, imagem, fps, desenho=True):
+        # Converter a cor da imagem (o Mediapipe usa somente imagens em RGB e o OpenCV captura em BGR)
+        imagem_rgb = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
+
+        # Resultado do processamento da imagem
+        self.resultados = self.pose.process(imagem_rgb)
+        
+        # Colocar as landmarks no corpo
+        if self.resultados.pose_landmarks:
+            if desenho:
+                self.mp_desenho.draw_landmarks(imagem, self.resultados.pose_landmarks,
+                                               self.mpPose.POSE_CONNECTIONS,
+                                         self.mp_desenho.DrawingSpec(color=(0, 0, 255)),
+                                         self.mp_desenho.DrawingSpec(color=(0, 255, 0)))
+                
+        # Colcar o valor de FPS na tela
+        cv2.putText(imagem, str(int(fps)), (10, 40), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+        return imagem
+                
+    def encontrar_posicao_camera(self, imagem, desenho=True):
+        lista_landmark = []
+        if self.resultados.pose_landmarks:
+            for item, landmark in enumerate (self.resultados.pose_landmarks.landmark):
+                # Pegar o valor do pixel onde cada landmark está
+                altura, largura, canal = imagem.shape
+                cx, cy = int(landmark.x*largura), int(landmark.y*altura)
+                lista_landmark.append([item, cx, cy])
+                if desenho:
+                    cv2.circle(imagem, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
+    
+        return lista_landmark
+            
+    def encontrar_pose_video(self, imagem, fps, desenho=True):
+        # Redimensionar a escala dos vídeos se estiverem maiores do que a tela
+        altura = int(imagem.shape[0] * self.escala / 100)
+        largura = int(imagem.shape[1] * self.escala / 100)
+        dimensao = (largura, altura)
+
+        imagem_redimensionada = cv2.resize(imagem, dimensao, interpolation=cv2.INTER_AREA)
+
+        # Converter a cor da imagem (o Mediapipe usa somente imagens em RGB e o OpenCV captura em BGR)
+        imagem_rgb = cv2.cvtColor(imagem_redimensionada, cv2.COLOR_BGR2RGB)
+
+        # Resultado do processamento da imagem
+        self.resultados = self.pose.process(imagem_rgb)
+
+        # Colocar as landmarks no corpo
+        if self.resultados.pose_landmarks:
+            if desenho:
+                self.mp_desenho.draw_landmarks(imagem_redimensionada, self.resultados.pose_landmarks,
+                                               self.mpPose.POSE_CONNECTIONS,
+                                         self.mp_desenho.DrawingSpec(color=(0, 0, 255)),
+                                         self.mp_desenho.DrawingSpec(color=(0, 255, 0)))
+                
+        # Colocar o valor de FPS na tela
+        cv2.putText(imagem_redimensionada, str(int(fps)), (10, 40), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0),3)
+
+        return imagem_redimensionada
+    
+    def encontrar_posicao_video(self, imagem_redimensionada, desenho=True):
+        lista_landmark = []
+        if self.resultados.pose_landmarks:
+            for item, landmark in enumerate (self.resultados.pose_landmarks.landmark):
+                # Pegar o valor do pixel onde cada landmark está
+                altura, largura, canal = imagem_redimensionada.shape
+                cx, cy = int(landmark.x*largura), int(landmark.y*altura)
+                cv2.circle(imagem_redimensionada, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
+
+        return lista_landmark
+    
+
+def main(video=0):
+    tempo_anterior = 0
+    tempo_atual = 0
+    detector = DetectorPose()
+    
+    # Informar a pasta de onde está o vídeo. Se quiser usar a câmera, basta passar 0 como argumento ao invés da pasta do vídeo
+    cap = cv2.VideoCapture(video)
+    
+    while True:
+        sucesso, imagem = cap.read()
+
+        video_str = str(video)
+        
+        # Configurar o FPS da captura
+        tempo_atual = time.time()
+        fps = 1 / (tempo_atual - tempo_anterior)
+        tempo_anterior = tempo_atual
+        
+        if '.mp4' in video_str:
+            imagem = detector.encontrar_pose_video(imagem, fps)
+            lista_landmark_video = detector.encontrar_posicao_video(imagem)
+            
+            #Mostrar imagem na tela
+            cv2.imshow('Imagem vídeo', imagem)
+            
+        else:
+            imagem = detector.encontrar_pose_camera(imagem, fps)
+            lista_landmark_camera = detector.encontrar_posicao_camera(imagem)
+            
+            # Mostrar imagem na tela
+            cv2.imshow('Imagem câmera', imagem)
+
+        # Terminar o loop
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            break
+        
+    # Fechar a tela de captura
+    cap.release()
+    cv2.destroyAllWindows()
+
+    
+if __name__ == '__main__':
+    main()
+
